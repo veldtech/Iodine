32000484ca524afd588f1bc60a815cb170f2e6ec
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-10-09 22:06:41 -0400

    feat(api): Implement more endpoints (channels)

--- Iodine/Program.cs
+++ Iodine/Program.cs
@@ -5,17 +5,14 @@ using System.Text.Json.Nodes;
 using CommunityToolkit.HighPerformance;
 using CommunityToolkit.HighPerformance.Buffers;
 using Iodine.PolyfillHelper;
-using Microsoft.AspNetCore.Http.Json;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
-using Polly;
 using Remora.Discord.API;
 using Remora.Discord.API.Abstractions.Objects;
 using Remora.Discord.API.Abstractions.Rest;
 using Remora.Discord.Caching;
 using Remora.Discord.Caching.Abstractions;
 using Remora.Discord.Caching.Abstractions.Services;
-using Remora.Discord.Caching.Redis.Extensions;
 using Remora.Discord.Caching.Redis.Services;
 using Remora.Discord.Caching.Services;
 using Remora.Discord.Rest;
@@ -51,115 +48,213 @@ services.ConfigureHttpJsonOptions(options => options.SerializerOptions.Converter
 
 var app = builder.Build();
 
-app.MapGet
+// This is what will thunk API requests
+app.MapFallback
 (
-    "/channels/{channelID}/messages/{messageID}",
-    async (HttpContext context, IRestHttpClient rest, ICacheProvider cache, Snowflake channelID, Snowflake messageID) =>
+    async (HttpContext context, IRestHttpClient rest) =>
     {
-        var cacheResult = await cache.RetrieveAsync<IMessage>(new KeyHelpers.MessageCacheKey(channelID, messageID));
-
-        if (!cacheResult.IsSuccess)
+        var restResult = await (context.Request.Method switch
         {
-            var fetch = await rest.GetAsync<IMessage>($"channels/{channelID}/messages/{messageID}");
+            "GET"    => rest.GetAsync<JsonNode>(context.Request.Path),
+            "PUT"    => rest.PutAsync<JsonNode>(context.Request.Path),
+            "POST"   => rest.PostAsync<JsonNode>(context.Request.Path),
+            "PATCH"  => rest.PatchAsync<JsonNode>(context.Request.Path),
+            "DELETE" => rest.DeleteAsync<JsonNode>(context.Request.Path),
+            _        => Task.FromResult(Result<JsonNode>.FromError(new InvalidOperationError("Unknown request method"))),
+        });
 
-            if (!fetch.IsSuccess)
-            {
-                await HandleRestErrorAsync(context, fetch.Error);
-
-                return;
-            }
-
-            await RespondAsync(context, fetch.Entity);
+        if (restResult.IsSuccess)
+        {
+            await ResponseHelper<JsonNode>.RespondAsync(context, restResult.Entity);
         }
+        else
+        {
+            await ResponseHelper<JsonNode>.HandleRestErrorAsync(context, restResult.Error);
+        }
+
     }
 );
 
+// Gets a message from the API.
+app.MapGet
+(
+    "/channels/{channelID}/messages/{messageID}",
+    (HttpContext context, IRestHttpClient rest, ICacheProvider cache, ulong channelID, ulong messageID) 
+        => ResponseHelper<IMessage>.HandleRequestAsync
+        (
+            context,
+            rest,
+            cache,
+            static (IMessage? _, (ulong cid, ulong mid) state) => new KeyHelpers.MessageCacheKey(new(state.cid), new(state.mid)),
+            (channelID, messageID)
+        )
+);
+
+// Posts a message to a channel, caching the result.
 app.MapPost
 (
-    "/channels/{channelID}/messages/", 
-    async (HttpContext context, IDiscordRestChannelAPI rest, ICacheProvider cache, Snowflake channelID) =>
-    {
-        var jsonOptions = context.RequestServices.GetRequiredService<IOptionsMonitor<JsonSerializerOptions>>().Get("Discord");
-        var message = await GetJsonNodesFromRequestAsync(context);
-
-        var postResult = await rest.CreateMessageAsync
+    "/channels/{channelID}/messages/",
+    (HttpContext context, IRestHttpClient rest, ICacheProvider cache, ulong channelID)
+        => ResponseHelper<IMessage>.HandleRequestAsync
         (
-            channelID,
-            message["content"].Deserialize<Optional<string>>(jsonOptions),
-            embeds: message["embeds"].Deserialize<Optional<IReadOnlyList<IEmbed>>>(jsonOptions),
-            allowedMentions: message["allowed_mentions"].Deserialize<Optional<IAllowedMentions>>(jsonOptions),
-            messageReference: message["message_reference"].Deserialize<Optional<IMessageReference>>(jsonOptions),
-            components: message["components"].Deserialize<Optional<IReadOnlyList<IMessageComponent>>>(jsonOptions)
-        );
+            context,
+            rest,
+            cache,
+            static (message, cid) => new KeyHelpers.MessageCacheKey(message!.ChannelID, message.ID),
+            channelID
+        )
+);
 
-        if (!postResult.IsSuccess)
-        {
-            await HandleRestErrorAsync(context, postResult.Error);
+app.MapPatch
+(
+    "/channels/{channelID}/messages/{messageID}",
+    (HttpContext context, IRestHttpClient rest, ICacheProvider cache, ulong channelID, ulong messageID)
+        => ResponseHelper<IMessage>.HandleRequestAsync
+        (
+            context,
+            rest,
+            cache,
+            static (IMessage? _, (ulong cid, ulong mid) state) => new KeyHelpers.MessageCacheKey(new(state.cid), new(state.mid)),
+            (channelID, messageID)
+        )
+);
 
-            return;
-        }
-
-        await RespondAsync(context, postResult.Entity);
-
-        await CacheAsync(context, cache, postResult.Entity, new KeyHelpers.MessageCacheKey(channelID, postResult.Entity.ID));
-    }
+app.MapDelete
+(
+    "/channels/{channelID}/messages/{messageID}",
+    (HttpContext context, IRestHttpClient rest, ICacheProvider cache, ulong channelID, ulong messageID)
+        => ResponseHelper<IMessage>.HandleRequestAsync
+        (
+            context,
+            rest,
+            cache,
+            static (IMessage? _, (ulong cid, ulong mid) state) => new KeyHelpers.MessageCacheKey(new(state.cid), new(state.mid)),
+            (channelID, messageID)
+        )
 );
 
 app.Run();
 
 return;
 
-static async Task CacheAsync<T>(HttpContext context, ICacheProvider provider, T entity, CacheKey cacheKey) where T : class
+// A generic helper method that encapsulates common logic between endpoints.
+file static class ResponseHelper<TEntity> where TEntity : class
 {
-    var cacheEntity = entity;
-    var existsResult = await provider.RetrieveAsync<T>(cacheKey);
-    var cacheOptions = context.RequestServices.GetRequiredService<ImmutableCacheSettings>();
-    
-    if (existsResult.IsSuccess)
+    public static async Task HandleRequestAsync<TState> 
+    (
+        HttpContext context,
+        IRestHttpClient rest,
+        ICacheProvider cache,
+        Func<TEntity?, TState?, CacheKey> cacheKey,
+        TState? state = default
+    )
     {
-        cacheEntity = CacheHelper<T>.FastPolyFill(existsResult.Entity, entity);
+        var requestMethod = context.Request.Method;
+
+        if (requestMethod is "GET")
+        {
+            var cacheResult = await cache.RetrieveAsync<TEntity>(cacheKey(null, state));
+
+            if (cacheResult.IsSuccess)
+            {
+                await RespondAsync(context, cacheResult.Entity);
+
+                return;
+            }
+        }
+    
+        var restResult = await (requestMethod switch
+        {
+            "GET"    => rest.GetAsync<TEntity>(context.Request.Path),
+            "PUT"    => rest.PutAsync<TEntity>(context.Request.Path, allowNullReturn: true),
+            "POST"   => rest.PostAsync<TEntity>(context.Request.Path),
+            "PATCH"  => rest.PatchAsync<TEntity>(context.Request.Path, allowNullReturn: true),
+            "DELETE" => rest.DeleteAsync<TEntity>(context.Request.Path, allowNullReturn: true),
+            _        => Task.FromResult(Result<TEntity>.FromError(new InvalidOperationError("Unknown request method"))),
+        });
+
+        if (!restResult.IsDefined(out var res))
+        {
+            await HandleRestErrorAsync(context, restResult.Error!);
+
+            return;
+        }
+
+        await RespondAsync(context, res);
+
+        if (requestMethod is "DELETE")
+        {
+            await cache.EvictAsync(cacheKey(null, state));
+        }
+        else
+        {
+            var cacheResult = await cache.RetrieveAsync<TEntity>(cacheKey(res, state));
+
+            TEntity? cacheEntity = !cacheResult.IsDefined(out cacheEntity) ? res : CacheHelper<TEntity>.FastPolyFill(res, cacheEntity);
+            await CacheAsync(context, cache, cacheEntity, cacheKey(res, state));
+        }
     }
     
-    await provider.CacheAsync(cacheKey, cacheEntity, cacheOptions.GetEntryOptions<T>());
-}
-
-static async Task<JsonNode> GetJsonNodesFromRequestAsync(HttpContext context)
-{
-    using var bufferWriter = new ArrayPoolBufferWriter<byte>();
-    await using var bufferStream = bufferWriter.AsStream();
-    
-    await context.Request.Body.CopyToAsync(bufferStream);
-
-    var content = JsonNode.Parse(bufferWriter.WrittenSpan);
-
-    return content;
-}
-
-static async Task HandleRestErrorAsync(HttpContext httpContext, IResultError fetchError)
-{
-    var (statusCode, data) = fetchError switch
+    static async Task CacheAsync(HttpContext context, ICacheProvider provider, TEntity entity, CacheKey cacheKey)
     {
-        RestResultError<IRestError> re => ((int)HttpStatusCode.BadRequest, re.Message),
-        HttpResultError http           => ((int)http.StatusCode, http.Message),
-        _                              => throw new UnreachableException("The rest client only returns two types of errors."),
-    };
+        var cacheEntity = entity;
+        var existsResult = await provider.RetrieveAsync<TEntity>(cacheKey);
+        var cacheOptions = context.RequestServices.GetRequiredService<ImmutableCacheSettings>();
+        
+        if (existsResult.IsSuccess)
+        {
+            cacheEntity = CacheHelper<TEntity>.FastPolyFill(existsResult.Entity, entity);
+        }
+        
+        await provider.CacheAsync(cacheKey, cacheEntity, cacheOptions.GetEntryOptions<TEntity>());
+    }
 
-    httpContext.Response.StatusCode = statusCode;
-    await httpContext.Response.WriteAsJsonAsync(new { error = data });
-}
+    public static async Task<JsonNode> GetJsonNodesFromRequestAsync(HttpContext context)
+    {
+        using var bufferWriter = new ArrayPoolBufferWriter<byte>();
+        await using var bufferStream = bufferWriter.AsStream();
+        
+        await context.Request.Body.CopyToAsync(bufferStream);
 
-static async Task RespondAsync<TValue>(HttpContext context, TValue payload)
-{
-    // This is very fast and very efficient. See links for info:
-    // https://github.com/Remora/Remora.Discord/pull/318#issuecomment-1745328751
-    // https://github.com/Remora/Remora.Discord/blob/434c68a/Backend/Remora.Discord.Gateway/Transport/WebSocketPayloadTransportService.cs#L170-L179
-    
-    var jsonOptions = context.RequestServices.GetRequiredService<IOptionsMonitor<JsonSerializerOptions>>().Get("Discord");
-    
-    using var bufferWriter = new ArrayPoolBufferWriter<byte>(256);
-    await using var jsonWriter = new Utf8JsonWriter(bufferWriter, new JsonWriterOptions { Encoder = jsonOptions.Encoder, SkipValidation = !Debugger.IsAttached });
+        var content = JsonNode.Parse(bufferWriter.WrittenSpan)!;
 
-    JsonSerializer.Serialize(jsonWriter, payload, jsonOptions);
+        return content;
+    }
 
-    bufferWriter.WrittenSpan.CopyTo(context.Response.BodyWriter.GetSpan(bufferWriter.WrittenCount));
+    public static async Task HandleRestErrorAsync(HttpContext httpContext, IResultError fetchError)
+    {
+        var (statusCode, data) = fetchError switch
+        {
+            RestResultError<IRestError> re => ((int)HttpStatusCode.BadRequest, re.Message),
+            HttpResultError http           => ((int)http.StatusCode, http.Message),
+            _                              => throw new UnreachableException("The rest client only returns two types of errors."),
+        };
+
+        httpContext.Response.StatusCode = statusCode;
+        await httpContext.Response.WriteAsJsonAsync(new { error = data });
+    }
+
+    public static async Task RespondAsync<TValue>(HttpContext context, TValue payload)
+    {
+        // This is very fast and very efficient. See links for info:
+        // https://github.com/Remora/Remora.Discord/pull/318#issuecomment-1745328751
+        // https://github.com/Remora/Remora.Discord/blob/434c68a/Backend/Remora.Discord.Gateway/Transport/WebSocketPayloadTransportService.cs#L170-L179
+        
+        var jsonOptions = context.RequestServices.GetRequiredService<IOptionsMonitor<JsonSerializerOptions>>().Get("Discord");
+        
+        using var bufferWriter = new ArrayPoolBufferWriter<byte>(256);
+        await using var jsonWriter = new Utf8JsonWriter(bufferWriter, new JsonWriterOptions { Encoder = jsonOptions.Encoder, SkipValidation = !Debugger.IsAttached });
+
+        if (payload is JsonNode jn) // Our fallback calls into ResponseHelper<JsonNode>
+        {
+            jn.WriteTo(jsonWriter, jsonOptions);
+        }
+        else
+        {
+            
+            JsonSerializer.Serialize(jsonWriter, payload, jsonOptions);
+        }
+
+        bufferWriter.WrittenSpan.CopyTo(context.Response.BodyWriter.GetSpan(bufferWriter.WrittenCount));
+    }
 }
