8626a1f5f3f5bb25841df8984eb94f75dc513ffa
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-10-11 14:33:21 -0400

    refactor(cache): Use tenant cache keys sparingly
    This commit allows for the sharing of some common resources (such as users) across multiple tenants.
    
    BREAKING CHANGE: This commit changes it so that tenants may share resources deemed to be safe for multi-tenant use; entities such as guilds are keyed by the tenant's ID.

--- Iodine/Services/RedisTenantCacheWrapper.cs
+++ Iodine/Services/RedisTenantCacheWrapper.cs
@@ -1,8 +1,5 @@
 ﻿using System.Buffers.Binary;
-using System.Numerics;
-using System.Runtime.CompilerServices;
 using System.Text;
-using Remora.Discord.API;
 using Remora.Discord.Caching.Abstractions;
 using Remora.Discord.Caching.Abstractions.Services;
 using Remora.Discord.Rest;
@@ -16,19 +13,33 @@ public class RedisTenantCacheWrapper(ICacheProvider _actual, ITokenStore _token)
     private readonly Snowflake _tokenID = ConvertTokenToSnowflake(_token.Token);
 
     public ValueTask CacheAsync<TInstance>(CacheKey key, TInstance instance, CacheEntryOptions options, CancellationToken ct = default)
-        where TInstance : class => _actual.CacheAsync(new TenantCacheKey(_tokenID, key), instance, options, ct);
+        where TInstance : class => _actual.CacheAsync(CreateAppropriateCacheKey(_tokenID, key), instance, options, ct);
 
     public ValueTask<Result<TInstance>> RetrieveAsync<TInstance>(CacheKey key, CancellationToken ct = default)
-        where TInstance : class => _actual.RetrieveAsync<TInstance>(new TenantCacheKey(_tokenID, key), ct);
+        where TInstance : class => _actual.RetrieveAsync<TInstance>(CreateAppropriateCacheKey(_tokenID, key), ct);
 
     public ValueTask<Result> EvictAsync(CacheKey key, CancellationToken ct = default)
-        => _actual.EvictAsync(new TenantCacheKey(_tokenID, key), ct);
+        => _actual.EvictAsync(CreateAppropriateCacheKey(_tokenID, key), ct);
 
     public ValueTask<Result<TInstance>> EvictAsync<TInstance>(CacheKey key, CancellationToken ct = default) 
-        where TInstance : class => _actual.EvictAsync<TInstance>(new TenantCacheKey(_tokenID, key), ct);
+        where TInstance : class => _actual.EvictAsync<TInstance>(CreateAppropriateCacheKey(_tokenID, key), ct);
 
     private static Snowflake ConvertTokenToSnowflake(string token) 
         => new(BinaryPrimitives.ReadUInt64LittleEndian(Convert.FromBase64String(token.Split('.')[0]).AsSpan()));
+
+    private static CacheKey CreateAppropriateCacheKey(in Snowflake tenant, in CacheKey cacheKey)
+    {
+        var keyName = cacheKey.GetType().FullName!;
+
+        if (keyName.Contains("Guild") || keyName.Contains("Message"))
+        {
+            // Security: Always return a tenant cache key for guilds and messages.
+            // while a bot *could* theoretically be in the same server, under 90% of circumstances they aren't.
+            return new TenantCacheKey(tenant, cacheKey);
+        }
+
+        return cacheKey;
+    }
 }
 
 file record TenantCacheKey(Snowflake Tenant, CacheKey Inner) : CacheKey
